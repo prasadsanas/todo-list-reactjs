ef758ca1961ef64b5b90fddfca11201d1fbdc2ae
Prasad Sanas <sanasprasad98@gmail.com>
2021-12-12 03:03:30 +0530

    created form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import TodoList from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
